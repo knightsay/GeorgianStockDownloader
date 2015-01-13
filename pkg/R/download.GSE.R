3bccfbc9362c3fdfb716c154620aa187a762167f
knightsay <knightsay@hotmail.com>
2015-01-13 09:07:57 +0400

    Initial Upload

---
+++ pkg/R/download.GSE.R
@@ -0,0 +1,22 @@
+#' Download Stock Data from Georgian Stock Exchange Website
+#' 
+#' \code{download.GSE} takes a data frame of stock issues (stock IDs) and returns a data frame 
+#' of trade results
+#' 
+#' @param issues the stock IDs that we want trade results on
+#'   
+#' @return data frame containing the trade results of different stock issues from every day 
+#'         available on the website. 
+#'   
+#' %% @seealso  \link{download.FRED} \link{build.rates}
+#'   
+#'  %% @examples
+#' %%\dontrun{
+#' download.markit(start = as.Date("2005-12-31"), end = as.Date("2006-01-04"), 
+#'                 currency = "JPY")
+#' %%}       
+
+download.GSE <- function(issues){
+  
+  return(x)
+}
